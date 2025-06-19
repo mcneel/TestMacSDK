d83c405b3d95b7680f53c06710f437630b1ea0b3
Dan Rigdon-Bel <dan@mcneel.com>
2025-06-18 17:26:17 -0700

    Update make_rhino_debuggable.sh
    Allow for non-hardcoded paths

--- make_rhino_debuggable.sh
+++ make_rhino_debuggable.sh
@@ -1,29 +1,29 @@
+#!/usr/bin/env bash
+#First command-line arg should be full path to app bundle, e.g.: ./make_rhino_debuggable.sh ~/Desktop/RhinoWIP.app
 #Shell script to allow Rhino to be debugged for Rhino for Mac SDK plugins.
 
+TEMP_ENTITLEMENTS=$HOME/Desktop/rhino.entitlements
+
 #Get rid of any previous detritus
-unlink ~/rhino.entitlements
+unlink $TEMP_ENTITLEMENTS
 
 #Extract the entitlements for RhinoWIP to an XML file
-codesign --display --xml --entitlements ~/rhino.entitlements /Applications/RhinoWIP.app
+codesign --display --xml --entitlements $TEMP_ENTITLEMENTS $1
 
-if grep -wq "get-task-allow" ~/rhino.entitlements; then 
-
+if grep -wq "get-task-allow" $TEMP_ENTITLEMENTS; then 
 	echo "Rhino is already debuggable"
-
 else
-
 	#Replace a portion of the XML to inject the get-task-allow entitlement
-	sed -i.bak "s%<key>com.apple.security.cs.allow-jit</key><true/>%<key>com.apple.security.cs.allow-jit</key><true/><key>com.apple.security.get-task-allow</key><true/>%" ~/rhino.entitlements
+	sed -i.bak "s%<key>com.apple.security.cs.allow-jit</key><true/>%<key>com.apple.security.cs.allow-jit</key><true/><key>com.apple.security.get-task-allow</key><true/>%" $TEMP_ENTITLEMENTS
 
-	#cat ~/rhino.entitlements
-
+	#cat $TEMP_ENTITLEMENTS
+	
 	#Inject the modified entitlements into the RhinoApp bundle.
-	codesign -s - --deep --force --options=runtime --entitlements ~/rhino.entitlements /Applications/RhinoWIP.app
+	codesign -s - --deep --force --options=runtime --entitlements $TEMP_ENTITLEMENTS $1
 
 	echo "Rhino is now debuggable"
 
 fi
 
 #Clean up the working file
-unlink ~/rhino.entitlements
-
+unlink $TEMP_ENTITLEMENTS
